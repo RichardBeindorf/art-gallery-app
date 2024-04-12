45d6d69c9c7eff04f3893037e1efa2c08953c1fb
Richard Beindorf <richard_beindorf@web.de>
2024-04-12 15:21:20 +0200

    basic setup

---
+++ pages/components/index.js
@@ -0,0 +1,7 @@
+
+export default function ArtPieceList() {
+    
+    return 
+    // <ArtPiece> </ArtPiece>
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import useSWR from "swr"
+
 export default function HomePage() {
+  const { data, error, isLoading } = useSWR("https://example-apis.vercel.app/api/art")
+  console.log(data);
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Welcome to our - Art Gallery App!</h1>
+      <ul>
+        {data.map((pieces) => (
+          <li key={pieces.slug}>
+            <ArtPieceList pieces={pieces}/>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
