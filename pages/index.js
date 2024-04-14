7c824e19103c83e84517fba188803c9c162ffcaf
Eric <Karstenswobi@gmail.com>
2024-04-14 17:46:38 +0200

    display-all-images

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,32 @@
-import useSWR from "swr"
-import ArtPieceList from "./components/index"
+import useSWR from "swr";
+import ArtPieceList from "./components/index";
 
 export default function HomePage() {
-  const { data, error, isLoading } = useSWR("https://example-apis.vercel.app/api/art")
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art"
+  );
 
-  if (error) return <div>failed to load</div>
-  if (isLoading) return <div>loading...</div>
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
 
   console.log(data);
   return (
     <div>
       <h1>Welcome to our - Art Gallery App!</h1>
       <ul>
-        {data.map((piece) => (
-          <li key={piece.slug}>
-            <ArtPieceList pieces={piece} title={piece.name} artist={piece.artist} image={"https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg"}/>
-          </li>
-        ))}
+        {data.map((piece) => {
+          const imageUrl = piece.imageSource;
+          console.log("Image URL:", imageUrl);
+          return (
+            <li key={piece.slug}>
+              <ArtPieceList
+                title={piece.name}
+                artist={piece.artist}
+                image={imageUrl}
+              />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
