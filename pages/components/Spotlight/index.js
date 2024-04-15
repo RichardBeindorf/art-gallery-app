0373927b3234b0d01c19f8b650725d166d419c32
Eric <Karstenswobi@gmail.com>
2024-04-15 11:34:02 +0200

    modified-components/index.js

--- pages/components/Spotlight/index.js
+++ pages/components/Spotlight/index.js
@@ -1,18 +1,15 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-export default function Spotlight({ data }){
-
-    return <>
-    {data.map((piece) => {
-    return <div key={piece.slug}>
-    <Image 
-    src={piece.imageSource}
-    alt={`A paintig by the artist`}
-    height={200}
-    width={200}
-    />
-    <h6>Made by {piece.artist}</h6>
-    </div>
-    })}
+export default function Spotlight({ data }) {
+  return (
+    <>
+      {data.map((piece) => {
+        return (
+          <div key={piece.slug}>
+            <h6>Made by {piece.artist}</h6>
+          </div>
+        );
+      })}
     </>
+  );
 }
