25798fd2ba0097de5a9826cd44ce18a4a97515d3
Natascha Tenhagen <nattenhagen@googlemail.com>
2024-04-15 09:51:00 +0200

    add nav bar

---
+++ pages/pieces.js
@@ -0,0 +1,32 @@
+import ArtPieceList from "./components";
+import useSWR from "swr";
+
+export default function HomePage() {
+  const { data, error, isLoading } = useSWR(
+    "https://example-apis.vercel.app/api/art"
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  console.log(data);
+  return (
+    <div>
+      <h1>Welcome to our - Art Gallery App!</h1>
+      <ul>
+        {data.map((piece) => (
+          <li key={piece.slug}>
+            <ArtPieceList
+              pieces={piece}
+              title={piece.name}
+              artist={piece.artist}
+              image={
+                "https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg"
+              }
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ pages/spotlight.js
@@ -0,0 +1,5 @@
+export default function Spotlight() {
+  return <h1>Welcome to the Art Gallery</h1>;
+
+  // Hier noch die Sporlight Gallery einfügen
+}
